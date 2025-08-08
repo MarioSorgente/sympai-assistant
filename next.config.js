848c8201d74ede06a839259925ab7519686278fe
MarioSorgente <94972242+MarioSorgente@users.noreply.github.com>
2025-08-08 12:05:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ⬇️ PoC: don’t fail production builds on TS/ESLint
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  eslint: { ignoreDuringBuilds: true }
 };
 export default nextConfig;
