30e604cabe0ca33ef641419db19510613fa8ec94
MarioSorgente <94972242+MarioSorgente@users.noreply.github.com>
2025-08-08 12:16:05 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./app/**/*.{js,jsx}"],
-  theme: { extend: { colors: { brand: { DEFAULT: "#6E56CF" } } } },
-  darkMode: "class",
-  plugins: []
-};
